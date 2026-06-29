c5bf44672b199ffa55becf5dc19ff89b818f672d
Makoto Katsumata <mako10k@mk10.org>
2026-06-29 11:11:35 +0900

    Release secdat v0.4.2

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("0.4.1")
+        .atleast_version("0.4.2")
         .probe("libsecdat")
         .expect("pkg-config could not find libsecdat");
 }
