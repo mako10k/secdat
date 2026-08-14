7f8da714422a71fb72ded70c4374c17b18740818
Makoto Katsumata <mako10k@mk10.org>
2026-08-14 11:49:59 +0900

    release: prepare v0.8.0

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("0.7.0")
+        .atleast_version("0.8.0")
         .probe("libsecdat")
         .expect("pkg-config could not find libsecdat");
 }
--- tests/sdk_regression.sh
+++ tests/sdk_regression.sh
@@ -1423,7 +1423,7 @@ includedir=$source_root/src
 
 Name: libsecdat
 Description: C SDK for secdat secret access and session control
-Version: 0.7.0
+Version: 0.8.0
 Libs: -L\${libdir} -Wl,-rpath,\${libdir} -lsecdat
 Libs.private: $sdk_private_link_flags_text
 Cflags: -I\${includedir} -I$build_root/src
